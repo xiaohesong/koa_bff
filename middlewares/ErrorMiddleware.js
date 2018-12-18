33fd89ccf378a1c09f575130d33e0eb5c5629dc8
xiaohesong <didmehh@163.com>
2018-12-18 16:46:17 +0800

    throw error

--- middlewares/ErrorMiddleware.js
+++ middlewares/ErrorMiddleware.js
@@ -6,15 +6,16 @@ async function errorMiddleware(ctx, next){
       console.log('存在e.data', e.data, e.status);
       
       ctx.body = e.data
-      ctx.app.emit('error', e, ctx);
+      ctx.throw(e.data.code, e.data)
+      // ctx.app.emit('error', e, ctx);
       return
     }
     console.log('如果存在e.data, 这里就不会再运行')
     ctx.body = {code: e.status, msg: e.message || e};
-
+    ctx.throw(e.status, e.message || '错误，未捕获到message')
     if (e.status === 500) {
       // 触发 koa 统一错误事件，可以打印出详细的错误堆栈 log
-      ctx.app.emit('error', e, ctx);
+      // ctx.app.emit('error', e, ctx);
     }
   }
 } 
