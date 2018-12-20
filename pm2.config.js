fc9a03af6c1c07fc459652d5fc57929a4250f23a
xiaohesong <didmehh@163.com>
2018-12-20 16:09:31 +0800

    pm2 log add time format

--- pm2.config.js
+++ pm2.config.js
@@ -3,9 +3,10 @@ module.exports = {
   apps: [{
     name: "koa_bff",
     script: "./index.js",
+    log_date_format: "YYYY-MM-DD HH:mm Z",
     env: {
       NODE_ENV: "development",
-      PORT: 3000
+      PORT: 8080
     },
     env_staging: {
       NODE_ENV: "staging",
