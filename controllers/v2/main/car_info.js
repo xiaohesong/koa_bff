aaf803010e0020e56dbc9239277d7ce809f0d777
xiaohesong <didmehh@163.com>
2019-08-06 09:51:53 +0800

    save file

--- controllers/v2/main/car_info.js
+++ controllers/v2/main/car_info.js
@@ -4,6 +4,10 @@ var fs = require('fs');
 var os = require('os');
 const needTypes = ['cars', 'drivers'];
 
+function buildFolder(url) {
+  return path.resolve(__dirname, `${url}`)
+}
+
 async function index(ctx) {
   const uid = uuidv1()
   const file = ctx.request.files.file
@@ -11,17 +15,26 @@ async function index(ctx) {
   if(!needTypes.includes(type)){
     throw new Error('Unvalid type')
   }
-  const basicPath = `../../../files/${type}`
-  const dir = path.resolve(__dirname, `${basicPath}`)
-  if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+  saveFile({file, type, uid})
+  
+}
+
+function saveFile({file, type, uid}) {
+  const basicPath = `../../../files/`
+  const filesPath = buildFolder(basicPath)
+  if (!fs.existsSync(filesPath)){
+    console.log('不存在的地址', filesPath);
+    fs.mkdirSync(filesPath);
+  }
+  const typePath = buildFolder(`${basicPath}${type}`)
+  if (!fs.existsSync(typePath)){
+    console.log('不存在的地址', typePath);
+    fs.mkdirSync(typePath);
   }
   const ext = file.name.split('.').pop()
-  
   const reader = fs.createReadStream(file.path);
-  const stream = fs.createWriteStream(path.resolve(__dirname, `${basicPath}/${uid}.${ext}`));
+  const stream = fs.createWriteStream(path.resolve(__dirname, `${basicPath}${type}/${uid}.${ext}`));
   reader.pipe(stream);
-  
 }
 exports.car_info = index
 
